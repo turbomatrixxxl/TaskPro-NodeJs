fe7c028295dad71c9b04928c129feebdedd10f2a
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-02-01 15:29:16 +0200

    updated s3Client.js request with the header token

--- src/config/s3Client.js
+++ src/config/s3Client.js
@@ -1,18 +1,19 @@
 const AWS = require("aws-sdk");
 require("dotenv").config();
 
-// Set your Cloudflare R2 credentials here using the environment variables
-const ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
-const SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
-const REGION = process.env.R2_REGION; // Region set to 'eu'
-
+// Set up the S3 client to interact with Cloudflare R2
 const s3Client = new AWS.S3({
-  accessKeyId: ACCESS_KEY_ID,
-  secretAccessKey: SECRET_ACCESS_KEY,
-  region: REGION,
-  endpoint: process.env.R2_ENDPOINTS, // R2 endpoint from the .env file
+  accessKeyId: process.env.R2_ACCESS_KEY_ID,
+  secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  region: process.env.R2_REGION,
+  endpoint: process.env.R2_ENDPOINTS, // Ensure this matches your Cloudflare R2 endpoint
   signatureVersion: "v4",
   s3BucketEndpoint: true,
+  httpOptions: {
+    headers: {
+      Authorization: `Bearer ${process.env.R2_TOKEN}`, // Adding the R2_TOKEN from .env
+    },
+  },
 });
 
 module.exports = { s3Client };
